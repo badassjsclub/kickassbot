4bee2c7e620448fe1fcb4a71256ca02400d1b7ca
Ye Liu <ye@basecase.co>
2016-06-01 02:09:34 -0400

    Changed default message

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	log.Printf("Kickass Bot (v%s) is starting on port %s\n", VERSION, port)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Kickass Bot is coming soon!")
+		fmt.Fprintf(w, "Kickass Bot (v%s) is in service.", VERSION)
 	})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
